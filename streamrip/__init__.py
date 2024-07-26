0edb61e85ce0aa013a3eea5745c866d8398d4a46
lymrvie <layladbug@icloud.com>
2024-07-26 00:51:36 -0400

    u

--- streamrip/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import converter, db, exceptions, media, metadata
-from .config import Config
-
-__all__ = ["Config", "media", "metadata", "converter", "db", "exceptions"]
-__version__ = "2.0.5"
